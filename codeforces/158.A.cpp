b92cc729386753dc589baa5c5f62b9d587c9131d
Victor <frivenman@gmail.com>
2016-07-25 14:31:46 +0400

    task 4 solved

---
+++ codeforces/158.A.cpp
@@ -0,0 +1,32 @@
+//here is my task  http://codeforces.com/problemset/problem/158/A
+#include<iostream>
+using namespace std;
+ 
+int main ()
+{
+	int n,k;
+	cin >> n >> k;
+	int a[55];
+    
+	for(int i=0; i<n;i++ )
+	{
+	    cin >> a[i];
+	}
+	
+	
+	int count = 0;
+	
+	for(int i=0; i<n; ++i)
+	{
+		if (a[i]>= a[k-1] && a[i]>0)
+		{
+		    count++;
+		}
+	
+ 	}
+	cout << count;
+	
+    return 0;
+}
+ 
+ 
