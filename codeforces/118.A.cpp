7676750088d36f3c7dece0e09bce352d702046f5
Victor <frivenman@gmail.com>
2016-07-25 17:56:26 +0400

    task five solved

---
+++ codeforces/118.A.cpp
@@ -0,0 +1,29 @@
+//here is my task  http://codeforces.com/problemset/problem/118/A
+#include<iostream>
+#include<ctype>
+using namespace std;
+ 
+int main () {
+    string a;
+
+    cin>>a;
+	for(int i=0; i<a.size(); ++i) 
+	{
+		
+		//int len = s.size();
+
+		if(a[i]=='a' || a[i]=='o' 
+		    || a[i]=='y' || a[i]=='e' 
+		    || a[i]=='u' || a[i]=='i'
+            || a[i]=='Y' || a[i]=='E' 
+		    || a[i]=='U' || a[i]=='I'			)
+		{
+		        cout<<"";
+		} else 
+		{ 
+		    cout<<"."<<tolower(a[i]);
+		}
+	}
+
+    return 0;
+}
