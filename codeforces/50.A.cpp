4567db31fce0033cecfbdb33602014bdb0f7400c
Victor <frivenman@gmail.com>
2016-07-26 00:28:23 +0400

    task number seven solved (first task solved by my self

---
+++ codeforces/50.A.cpp
@@ -0,0 +1,22 @@
+//here is my task http://codeforces.com/problemset/problem/50/A
+#include<iostream>
+#include<string>
+#include <cmath>
+
+using namespace std;
+
+int main () { 
+    int M,N;
+	
+	int dom;
+    
+	cin >> M >> N;
+	
+	
+   
+	
+	
+	 std::cout << floor((M*N)/2) << std::endl;	 ; 
+	
+	    return 0;
+}
