64a289274f2c1c56575c0e521b521b86aa0f093f
Victor <frivenman@gmail.com>
2016-07-26 09:46:39 +0400

    task has mistakes

---
+++ codeforces/231.A.cpp
@@ -0,0 +1,26 @@
+//here is my task http://codeforces.com/problemset/problem/231/A
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	int n; 
+	cin>>n;
+	std::string s[1];
+	int a=0;//count right answers
+	for(int i=0;n=i;i++){
+	    cin >> s >> s >> s >>endl;
+		if (s+s+s>1) {
+		    ++a;
+		} else{a=a;
+		}
+		
+	}
+	 cout<<a;
+	return 0 ;
+	}
+	
+	
+	
