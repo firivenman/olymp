200d991bedb5b2a0364862ce28b33235aea1f436
Victor <frivenman@gmail.com>
2016-07-27 00:55:07 +0400

    8 problem solved

--- codeforces/282.A.cpp
+++ codeforces/282.A.cpp
@@ -7,27 +7,21 @@ using namespace std;
 
 int main() 
 {
-    int n,x,i;
+    int n,x;
     x=0;
-	i=0;
-	cin>>n;
-	for( i=0;i<=n;i++);
-	{	
-	    if( i%2>0 )
-		{
-			x--;	
-		}
-		else if(i%2==0 && i/2>0)
-		{
-		    x++;
-		}else if (i/2==0)
-		{
-			x;
-		}
-	}
+    cin>>n;
+    string s;
+	for(int i=0;i<n;i++){
+	    cin >>s;
+	    if (s[1]=='+')
+	    {   
+	        x++;
+        } 
+        else 
+        {
+		    x--;
+	    }
+    }
 	cout << x;
 	return 0;
 }
-	
-	
-     
